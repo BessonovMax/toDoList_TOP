b7d5f62bab1361cb9ce9b7639bbd7109bb2fc4cc
Max Bessonov <max8essonov@gmail.com>
2025-05-02 16:15:30 +0500

    Initial commit

---
+++ webpack.prod.js
@@ -0,0 +1,40 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      // Image files we reference in our HTML template, e.g. as the src of an <img>
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      // Images we use in our JavaScript, where we will need to import the files
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
